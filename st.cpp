ad8c0dd8f4b14ac845f619d63f191c7e800b1152
Egor Ivanov <egorivanov212005@mail.ru>
2023-02-12 16:00:05 +0300

    2^k+2^n

--- st.cpp
+++ st.cpp
@@ -2,11 +2,19 @@
 int main(int argc, char* argv[]){
 	int k;
         std::cin>>k;
+	int n;
+	std::cin>>n;
 	unsigned int a;
 	a=1;
 	unsigned int b;
 	b=a<<k;
-	std::cout<<b<<std::endl;
+	unsigned int c;
+	c=1;
+	unsigned int s;
+	s=c<<n;
+	unsigned int d;
+	d=b+s;
+	std::cout<<d<<std::endl;
 	return 0;
 }
 
