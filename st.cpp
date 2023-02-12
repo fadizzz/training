8e2a54df7c30f17b0f4f9b582567672ca7fd71bd
Egor Ivanov <egorivanov212005@mail.ru>
2023-02-12 15:38:44 +0300

    2^k

---
+++ st.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(int argc, char* argv[]){
+	int k;
+        std::cin>>k;
+	unsigned int a;
+	a=1;
+	unsigned int b;
+	b=a<<k;
+	std::cout<<b<<std::endl;
+	return 0;
+}
+
