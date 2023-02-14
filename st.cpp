0ad4a4fefc908ce35ae8be3a78152cab9ef17ba7
Egor Ivanov <egorivanov212005@mail.ru>
2023-02-14 21:54:07 +0300

    value of k bit

--- st.cpp
+++ st.cpp
@@ -4,16 +4,17 @@ int main(int argc, char* argv[]){
         std::cin>>a;
 	signed int k;
 	std::cin>>k;
-	signed int h=1;
 	signed int b;
-	b=h<<k;
-	signed int s;
-	s=a|b;
-	signed int f;
-	f=~s;
-	signed int t;
-	t=~f;
-	std::cout<<t<<std::endl;
+	b=a>>k;
+	unsigned int s;
+	s=b;
+	unsigned int j;
+	j=s>>1;
+	unsigned int f;
+	f=j<<1;
+	unsigned int r;
+	r=b^f;
+	std::cout<<r<<std::endl;
 	return 0;
 }
 
